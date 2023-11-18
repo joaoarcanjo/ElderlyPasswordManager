82a4f2330690593c037eacc8d2fbe8664a8929a9
João Arcanjo <joaopedro.arcanjo@hotmail.com>
2023-11-18 12:10:02 +0000

    Delete ElderlyApp directory

--- ElderlyApp/src/realm/index.tsx
+++
@@ -1,6 +0,0 @@
-import {createRealmContext} from '@realm/react';
-import {Person, GeneratedPassword} from './Models';
-
-export const MovieRealmContext = createRealmContext({
-  schema: [Person, GeneratedPassword]  
-});
