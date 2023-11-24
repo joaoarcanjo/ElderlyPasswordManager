14077a4b73a211b0d7c86a3acd735a1118b3fbc0
João Arcanjo <joaopedro.arcanjo@hotmail.com>
2023-11-24 13:40:14 +0000

    Password avaliation added to create credential page.

--- elderlyApp/src/algorithms/zxcvbn/algorithm.ts
+++ elderlyApp/src/algorithms/zxcvbn/algorithm.ts
@@ -6,12 +6,21 @@ function zxcvbnTest() {
 }
 
 function zxcvbnMain(password: string) {
-    const passwordStrength = zxcvbn(password);
+    const passwordStrength = zxcvbn(password)
     
-    console.log(passwordStrength.score); // Output: 34
+    console.log(passwordStrength.score) 
     console.log(passwordStrength.guesses)
     console.log(passwordStrength.sequence)
     console.log(passwordStrength.feedback)
 }
 
-export default {zxcvbnMain, zxcvbn}
+//ALTEREI O ALGORITMO DE MODO A QUE APENAS RETORNE O NIVEL MÁXIMO SE O GUESSES_LOG > 13.5
+function getScore(password: string): number {
+    const passwordStrength = zxcvbn(password)
+    console.log(passwordStrength.guesses_log10)
+
+    if(passwordStrength.score == 4 && passwordStrength.guesses_log10 < 13.5) return 3
+    return passwordStrength.score
+}
+
+export {zxcvbnMain, getScore }
--- elderlyApp/src/features/add_credentials/actions/index.tsx
+++ elderlyApp/src/features/add_credentials/actions/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { TextInput, View, StyleSheet, Button, Text, TouchableOpacity } from "react-native";
 import { stylesButtons, stylesMainBox } from "../../../assets/styles/main_style";
 import { passwordFirstHalf, stylesAddCredential, stylesInputsCredencials } from "../styles/styles";
@@ -7,7 +7,8 @@ import Navbar from "../../../navigation/actions";
 import { addCredencial } from "../../../firebase/firestore/funcionalities";
 import { MaterialCommunityIcons, MaterialIcons } from '@expo/vector-icons'; 
 import { Entypo } from '@expo/vector-icons'; 
-
+import Algorithm from "../../password_generator/actions/algorithm";
+import { getScore } from '../../../algorithms/zxcvbn/algorithm'
 
 function MainBox() {
 
@@ -26,53 +27,67 @@ function CredentialsInput() {
     const [platform, setPlatform] = useState('');
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [avaliation, setAvaliation] = useState<number>(0)
 
     const [showPassword, setShowPassword] = useState(false); 
   
-    const handleSave = () => {
-      // Implementar lógica para salvar as informações
-      console.log('Plataforma:', platform);
-      console.log('Username:', username);
-      console.log('Password:', password);
-      // Adicione sua lógica de salvamento aqui (por exemplo, enviar para um servidor, salvar no estado global, etc.)
-        
-      addCredencial(platform, JSON.stringify({platform: platform, username: username, password: password}))
-    };
+    const handleSave = () => addCredencial(platform, JSON.stringify({platform: platform, username: username, password: password}))
 
-    const toggleShowPassword = () => { 
-        setShowPassword(!showPassword); 
-    };
+    const toggleShowPassword = () => setShowPassword(!showPassword);
+
+    const regeneratePassword = () => {
+        const password = Algorithm({length: 15, strict: true, symbols: true, uppercase: true, lowercase: true, numbers: true})
+        setPassword(password)
+    }
+
+    useEffect(() => {
+        setAvaliation(getScore(password))
+    }, [password])
+
+    useEffect(() => {
+        regeneratePassword()
+    }, [])
+
+    const AvaliationEmoji = () => {
+        switch(avaliation) {
+            case (0): return <Entypo name="emoji-sad" size={39} color="#cc0000" /> 
+            case (1): return <Entypo name="emoji-neutral" size={39} color="#ff3300" /> 
+            case (2): return <Entypo name="emoji-neutral" size={39} color="#e6b800" /> 
+            case (3): return <Entypo name="emoji-happy" size={39} color="#339933" /> 
+            default: return <Entypo name="emoji-flirt" size={39} color="#006600" /> 
+        }
+    }
 
     return (
 
         <View style={[{flex: 0.85}]}>
-            <View style={{ flex: 0.23, width: '95%', flexDirection: 'row'}}>
+            <View style={{ flex: 0.23, width: '100%', flexDirection: 'row'}}>
                 <View style={[{flex: 1, marginTop:'3%', marginHorizontal: '4%'}, stylesInputsCredencials.inputContainer]}>
-                    <Text numberOfLines={1} adjustsFontSizeToFit style={[{flex: 0.3, marginTop: '3%', marginLeft: '5%', width: '90%', justifyContent: 'center', fontSize: 20}]}>APP</Text>
+                    <Text numberOfLines={1} adjustsFontSizeToFit style={[{flex: 0.3, marginTop: '3%', marginLeft: '5%', width: '90%', justifyContent: 'center', fontSize: 20}]}>Platform</Text>
                     <View style={[{flex: 0.7, margin: '4%', flexDirection: 'row', alignItems: 'center', justifyContent: 'center'}, { borderRadius: 15, borderWidth: 2, backgroundColor: whiteBackgroud }]}>
                         <TextInput
                         placeholder="Insira a plataforma"
                         value={platform}
-                        style={{ flex: 0.8, fontSize: 17, marginRight: '5%' }}
+                        style={{ flex: 0.8, fontSize: 17 }}
                         onChangeText={text => setPlatform(text)}
                         />
                     </View>
                 </View>
             </View>
-            <View style={{ flex: 0.23, width: '95%', flexDirection: 'row'}}>
+            <View style={{ flex: 0.23, width: '100%', flexDirection: 'row'}}>
                 <View style={[{flex: 1, marginTop:'3%', marginHorizontal: '4%'}, stylesInputsCredencials.inputContainer]}>
                     <Text numberOfLines={1} adjustsFontSizeToFit style={[{flex: 0.3, marginTop: '3%', marginLeft: '5%', width: '90%', justifyContent: 'center', fontSize: 20}]}>Username</Text>
                     <View style={[{flex: 0.7, margin: '4%', flexDirection: 'row', alignItems: 'center', justifyContent: 'center'}, { borderRadius: 15, borderWidth: 2, backgroundColor: whiteBackgroud }]}>
                         <TextInput
                         placeholder="Insira o seu username"
                         value={username}
-                        style={{ flex: 0.8, fontSize: 17, marginRight: '5%' }}
+                        style={{ flex: 0.8, fontSize: 17 }}
                         onChangeText={text => setUsername(text)}
                         />
                     </View>
                 </View>
             </View>
-            <View style={{ flex: 0.4, width: '95%', flexDirection: 'row'}}>
+            <View style={{ flex: 0.4, width: '100%', flexDirection: 'row'}}>
                 <View style={[{flex: 1, marginTop:'3%', marginHorizontal: '4%'}, stylesInputsCredencials.inputContainer]}>
                     <Text numberOfLines={1} adjustsFontSizeToFit style={[{flex: 0.3, marginTop: '3%', marginLeft: '5%', width: '90%', justifyContent: 'center', fontSize: 20}]}>Password</Text>
                     <View style={[{flex: 0.7, margin: '4%', flexDirection: 'row', alignItems: 'center', justifyContent: 'center'}, { borderRadius: 15, borderWidth: 2, backgroundColor: whiteBackgroud }]}>
@@ -83,7 +98,7 @@ function CredentialsInput() {
                         secureTextEntry={!showPassword}
                         onChangeText={text => setPassword(text)}
                         />
-                        <Entypo name="emoji-sad" size={35} color="orange" />
+                        <AvaliationEmoji/>
                     </View>
                     
                     
@@ -91,7 +106,7 @@ function CredentialsInput() {
                         <TouchableOpacity style={[{flex: 0.35, marginRight: '2%', flexDirection: 'row'}, passwordFirstHalf.copyButton, stylesButtons.mainConfig]}  onPress={toggleShowPassword} >
                             <MaterialCommunityIcons name={showPassword ? 'eye-off' : 'eye'} size={35} color="black"/> 
                         </TouchableOpacity>
-                        <TouchableOpacity style={[{flex: 0.65, marginLeft: '2%'}, passwordFirstHalf.regenerateButton, stylesButtons.mainConfig]} onPress={() => {} }>
+                        <TouchableOpacity style={[{flex: 0.65, marginLeft: '2%'}, passwordFirstHalf.regenerateButton, stylesButtons.mainConfig]} onPress={() => {regeneratePassword()} }>
                             <Text numberOfLines={1} adjustsFontSizeToFit style={[{ fontSize: 22, fontWeight: 'bold', margin: '5%' }]}>Regenerar</Text>
                         </TouchableOpacity>
                     </View>
@@ -103,10 +118,6 @@ function CredentialsInput() {
     </View>
     )
 }
-//<Entypo name="emoji-flirt" size={24} color="black" />
-//<Entypo name="emoji-happy" size={24} color="black" />
-//<Entypo name="emoji-sad" size={35} color="orange" />
-//<Entypo name="emoji-neutral" size={30} color="black" />
 
 
 function AddCredencial() {
