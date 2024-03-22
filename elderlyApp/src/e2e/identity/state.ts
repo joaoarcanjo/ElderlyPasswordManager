497a15fd192417a5c42a9b3fe5057f90a3fc1d11
João Arcanjo <joaopedro.arcanjo@hotmail.com>
2024-03-22 21:20:54 +0000

    Delete elderlyApp directory

--- elderlyApp/src/e2e/identity/state.ts
+++
@@ -1,7 +0,0 @@
-import { SignalProtocolStore } from '../signal/signal-store'
-import { BehaviorSubject } from 'rxjs'
-import { SignalDirectory } from '../signal/signal-directory'
-
-export const directorySubject = new BehaviorSubject<SignalDirectory | null>(null)
-export const signalStore = new SignalProtocolStore()
-export const usernameSubject = new BehaviorSubject<string>('')
